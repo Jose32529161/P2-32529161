53ebec21dd6d07d6fed69be093ee21cb10579cce
Jose32529161 <131322338+Jose32529161@users.noreply.github.com>
2023-05-13 17:28:17 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	let name = 'Anibal Vilera'
+  res.render('index', {
+   title: 'P2-32529161', 
+   name: name,
+    });
+});
+
+router.post('/', function(req, res, next) {
+  let name = req.body.name;
+  let email = req.body.email;
+  let comment = req.body.comment;
+  let date = new Date(); // @todo falta formatear la fecha
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // @todo falta formatear la ip
+
+  db.insert(name, email, comment, date, ip);
+
+  res.redirect('/');
+});
+
+router.get('/contactos', function(req, res, next) {
+  db.select(function (rows) {
+    console.log(rows);
+  });
+  res.send('ok');
 });
 
 module.exports = router;
+
